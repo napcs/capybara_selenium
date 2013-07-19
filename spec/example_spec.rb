da6ca3dd3ccf1b733d098e470e02cae04f1b19dd
Brian Hogan <brianhogan@napcs.com>
2013-07-19 12:45:14 -0500

    Added example for popup prompt boxes

--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -1,15 +1,12 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
-describe "testing" do
-
-  it "loads the home page" do
-    visit "/index.html"
-    page.should have_content "I'm an HTML5 template"
+describe "dealing with popups" do
+  it "displays age that the user filled in" do
+    visit "/prompt.html"
+    prompt = page.driver.browser.switch_to.alert
+    prompt.send_keys('15')
+    prompt.accept
+    page.should have_content "You are 15 years old."
   end
 
-  it "should test Google" do
-    visit "http://www.google.com/"
-    fill_in "gbqfq", :with => "Capybara"
-    page.should have_content "jnicklas/capybara"
-  end
 end
